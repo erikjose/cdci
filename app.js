173a2f31d4aca14e941d1fc5fadd9fad0ebc791f
Erik <erik@wizzer.com.br>
2019-11-25 17:35:49 -0300

    Initial commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const server = express();
 
+server.get("/", (req, res) => res.send("ok"));
+
 server.listen(3001, () => {
   console.log("Servidor iniciado");
 });
